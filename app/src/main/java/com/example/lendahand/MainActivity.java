bf4fe07716d337ea88a214751032d733099ff690
Suraksha <surakshamotilal@gmail.com>
2020-06-19 19:15:00 +0200

    Pending request activity for admin completed with no errors

--- app/src/main/java/com/example/lendahand/MainActivity.java
+++ app/src/main/java/com/example/lendahand/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.lendahand;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
@@ -14,7 +15,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toast toast = Toast.makeText(getApplicationContext(), "Currently logged in as "+StayLoggedIn.getUserName(MainActivity.this), Toast.LENGTH_SHORT);
         toast.show();
-
+        Intent intent = new Intent(this, AdminPendingReqActivity.class);
+        startActivity(intent);
+        finish();
     }
 
 
